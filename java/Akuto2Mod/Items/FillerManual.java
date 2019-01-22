39c49265df09ec5126be0a32e2ac8a57ef162d67
akuto2 <akuto2.renraku@gmail.com>
2019-01-22 18:57:27 +0900

    マルチプレイ時にクラッシュする不具合を修正

--- java/Akuto2Mod/Items/FillerManual.java
+++ java/Akuto2Mod/Items/FillerManual.java
@@ -2,6 +2,8 @@ package Akuto2Mod.Items;
 
 import Akuto2Mod.Akuto2Core;
 import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import lib.manual.GuiManual;
 import lib.proxies.ClientProxy;
 import net.minecraft.entity.player.EntityPlayer;
@@ -18,11 +20,16 @@ public class FillerManual extends Item{
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
 		if(world.isRemote) {
-			player.openGui(Akuto2Core.instance, 4, world, 0, 0, 0);
-			FMLClientHandler.instance().displayGuiScreen(player, new GuiManual(stack, ClientProxy.getBookDataFromStack(stack)));
+			openBook(stack, world, player);
 		}
 
 		return stack;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void openBook(ItemStack stack, World world, EntityPlayer player){
+		player.openGui(Akuto2Core.instance, 4, world, 0, 0, 0);
+		FMLClientHandler.instance().displayGuiScreen(player, new GuiManual(stack, ClientProxy.getBookDataFromStack(stack)));
 	}
 
 	@Override
